3621db3891dc662f2d131c6aa9486a1cbc4c75ad
Jordan Carlile <jordan.carlile@gmail.com>
2019-02-10 11:13:51 +0000

    initial vm instance push, port and 0.0.0.0 host

--- ir_main.py
+++ ir_main.py
@@ -1,7 +1,7 @@
 import sys
 from common import commonPages
 from flask import Flask, session
-from flask_session import Session
+from flask_cors import CORS
 
 
 #-----------------Initialization---------------------------------
@@ -10,7 +10,7 @@ app.secret_key = "316tiantuan"
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SESSION_TYPE'] = 'filesystem'
 #waiting to be changed to suit firebase
-Session(app)
+CORS(app)
 
 #ssl_context = ('./ssl/server.crt', './ssl/server.key')
 
@@ -18,4 +18,4 @@ Session(app)
 app.register_blueprint(commonPages)
 #-----------------Running---------------------------------
 #app.run(host = "0.0.0.0", port = 8080, debug = True, threaded = True, ssl_context=ssl_context)
-app.run(host = "0.0.0.0", port = 8080, debug = True, threaded = True)
+app.run(host = "0.0.0.0", port = 80, debug = True, threaded = True)
