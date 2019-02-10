f55d2db73ff13f19c94c3e857cc078f7ef7a3958
Manav Aggarwal <manavaggarwal1234@gmail.com>
2019-02-10 05:31:17 -0800

    Restored get_summary

---
+++ ocr.py
@@ -0,0 +1,30 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'notes.jpg')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.types.Image(content=content)
+
+response = client.text_detection(image=image)
+
+texts = response.text_annotations
+
+print('Texts:')
+
+for text in texts:
+    print('{}'.format(text.description))
+    exit()
